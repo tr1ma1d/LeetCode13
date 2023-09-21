69c99299d5d69c10664b9dd531ea57ccb6f9ed5d
Nikita <musatovla3xnikita@gmail.com>
2023-09-21 22:08:28 +0300

    Добавьте файлы проекта.

---
+++ LeetCode13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int sum = 0;
+            Dictionary<char, int> romanNumbersDictionary = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentRomanChar = s[i];
+                romanNumbersDictionary.TryGetValue(currentRomanChar, out int num);
+                if (i + 1 < s.Length && romanNumbersDictionary[s[i + 1]] > romanNumbersDictionary[currentRomanChar])
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
